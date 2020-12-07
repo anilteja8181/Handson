786b8f5650b87ad7cd16426e5005093182e6a2bd
anilteja1414 <anilteja14@instance2.us-central1-a.c.modified-cairn-296907.internal>
2020-12-07 05:12:56 +0000

    add demo.sh

---
+++ demo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+a=30
+b=20
+echo Given values are $a and $b
+echo "Sum of $a and $b is :" $(( a + b ))
+echo "Diff of $a and $b is : " $(( a - b ))
+echo "Product of $a and $b is : "$(( a * b ))
+
