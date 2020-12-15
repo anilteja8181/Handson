45eeb265d4d3685b944884fcaaf66f39bb78d60b
anilteja8181 <75599291+anilteja8181@users.noreply.github.com>
2020-12-15 20:13:05 +0530

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -2,8 +2,9 @@
 
 a=30
 b=20
+c=5
 echo Given values are $a and $b
-echo "Sum of $a and $b is :" $(( a + b ))
+echo "Sum of $a and $b is :" $(( a + b + c ))
 echo "Diff of $a and $b is : " $(( a - b ))
 echo "Product of $a and $b is : "$(( a * b ))
 
